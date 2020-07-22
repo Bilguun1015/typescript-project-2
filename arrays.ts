df17be79ac079ca7461a3953e83092cfb9cd924f
Bilguun <bbg.1015@gmail.com>
2020-07-22 11:56:51 -0500

    benefits of typed arrays

--- arrays.ts
+++ arrays.ts
@@ -5,3 +5,15 @@ const arrayOfArrays: string[][] = [];
 
 const dates = [new Date(), new Date()];
 const carsByMake = [['f150'], ['corolla'], ['camaro']];
+
+// Help with inference when extracting values
+const aCar = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values to add
+carMakers.push(100);
+
+// Help with 'map' other methods
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
