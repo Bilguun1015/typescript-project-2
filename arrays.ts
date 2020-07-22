64c28928de164ad6d0d97c0ec166e58ca3a01ebb
Bilguun <bbg.1015@gmail.com>
2020-07-22 11:51:06 -0500

    learning when to use type annotations on arrays

---
+++ arrays.ts
@@ -0,0 +1,7 @@
+const carMakers = ['ford', 'toyota', 'chevy'];
+//use annotations when we initiliaze array with an empty array
+const newArray: string[] = [];
+const arrayOfArrays: string[][] = [];
+
+const dates = [new Date(), new Date()];
+const carsByMake = [['f150'], ['corolla'], ['camaro']];
