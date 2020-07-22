eec4641f7f7e14ee7bc8d12c0f794b5138fc72e7
Bilguun <bbg.1015@gmail.com>
2020-07-22 11:30:53 -0500

    learning more of function returns

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -13,3 +13,23 @@ function divide(a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
   return a * b;
 };
+
+const logger = (message: string): void => {
+  console.log(message);
+  // void can return undefined or null
+  // return undefined;
+  // return null;
+};
+
+//never -- exists the function early (it returns anything ever)
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+//common way to throw error
+const throwError1 = (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  }
+
+  return message;
+};
