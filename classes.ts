d2a41e09c3fb67162951653bc21cdbb764f0fc21
Bilguun <bbg.1015@gmail.com>
2020-07-22 15:27:52 -0500

    learned differences between public, private and protected

--- classes.ts
+++ classes.ts
@@ -1,18 +1,28 @@
 class Vehicle {
-  drive(): void {
-    console.log('chugga chugga');
-  }
+  // public drive(): void {
+  //   console.log('chugga chugga');
+  // }
 
-  honk(): void {
+  protected honk(): void {
     console.log('beep');
   }
 }
 
+const vehicle = new Vehicle();
+vehicle.honk();
+
 class Car extends Vehicle {
-  drive(): void {
+  // usage of prive only occurs if you dont want it to be accessed by other devs
+  // drive()can only be accessed in the class
+  private drive(): void {
     console.log('vroom');
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
   }
 }
 
 const mycar = new Car();
-mycar.drive();
+mycar.startDrivingProcess();
