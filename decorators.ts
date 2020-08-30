9c1d17bfa61b82ac40cfa74d88c311a4a0a21ee5
Bilguun <bbg.1015@gmail.com>
2020-08-30 12:39:45 -0500

    learned decorators and its usage

--- decorators.ts
+++ decorators.ts
@@ -1,19 +1,48 @@
+@classDecorator
 class Boat {
+  @testDecorator
   color: string = 'red';
 
-  // @testDecorator
+  @testDecorator
   get formattedColor(): string {
     return `This boat's color is ${this.color}`;
   }
 
-  pilot(): void {
-    console.log('swish');
+  @logError('Oops boat was sunk in ocean')
+  pilot(
+    @parameterDecorator speed: string,
+    @parameterDecorator generateWake: boolean
+  ): void {
+    if (speed === 'fast') {
+      console.log('swish');
+    } else {
+      console.log('nothing');
+    }
   }
 }
 
-function testDecorator(target: any, key: string): void {
-  console.log('Target:', target);
-  console.log('Key:', key);
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
 }
 
-testDecorator(Boat.prototype, 'pilot');
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
+}
+
+function testDecorator(target: any, key: string) {
+  console.log(key);
+}
+
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
+
+    desc.value = function () {
+      try {
+        method();
+      } catch (e) {
+        console.log(errorMessage);
+      }
+    };
+  };
+}
